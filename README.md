d93fe8f7df089d4e03aac500318334aca6f0d629
Kevin Ngo <me@ngokevin.com>
2016-04-19 18:04:46 -0700

    randomizer

--- README.md
+++ README.md
@@ -1,2 +1,69 @@
-# aframe-random-components
-Value randomizer components for A-Frame.
+## aframe-randomizer-components
+
+A-Frame randomizer components for [A-Frame](https://aframe.io).
+
+### Properties
+
+#### random-color
+
+| Property | Description         | Default Value |
+|----------|---------------------|---------------|
+| min      | Minimum RGB vector. | 0 0 0         |
+| max      | Maximum RGB vector. | 1 1 1         |
+
+#### random-position
+
+| Property | Description              | Default Value |
+|----------|--------------------------|---------------|
+| min      | Minimum position bounds. | -10 -10 -10   |
+| max      | Maximum position bounds. | 10 10 10      |
+
+#### random-rotation
+
+| Property | Description              | Default Value |
+|----------|--------------------------|---------------|
+| min      | Minimum rotation angles. | 0 0 0         |
+| max      | Maximum rotation angles. | 360 360 360   |
+
+#### random-scale
+
+| Property | Description               | Default Value |
+|----------|---------------------------|---------------|
+| min      | Minimum scale components. | 1 1 1         |
+| max      | Maximum scale components. | 2 2 2         |
+
+### Usage
+
+#### Browser Installation
+
+Install and use by directly including the [browser files](dist):
+
+```html
+<head>
+  <title>My A-Frame Scene</title>
+  <script src="https://aframe.io/releases/0.2.0/aframe.min.js"></script>
+  <script src="https://rawgit.com/ngokevin/aframe-randomizer-components/master/dist/aframe-randomizer-components.min.js"></script>
+</head>
+
+<body>
+  <a-scene>
+    <a-entity geometry="primitive: box"
+              random-position random-rotation random-scale></a-entity>
+  </a-scene>
+</body>
+```
+
+#### NPM Installation
+
+Install via NPM:
+
+```bash
+npm install aframe-randomizer-components
+```
+
+Then register and use.
+
+```js
+require('aframe');
+require('aframe-randomizer-components');
+```
---
+++ index.js
@@ -0,0 +1,87 @@
+if (typeof AFRAME === 'undefined') {
+  throw new Error('Component attempted to register before AFRAME was available.');
+}
+
+/**
+ * Set random color within bounds.
+ */
+AFRAME.registerComponent('random-color', {
+  schema: {
+    min: {default: {x: 0, y: 0, z: 0}, type: 'vec3'},
+    max: {default: {x: 1, y: 1, z: 1}, type: 'vec3'}
+  },
+
+  update: function () {
+    var data = this.data;
+    var max = data.max;
+    var min = data.min;
+    this.el.setAttribute('material', 'color', '#' + new THREE.Color(
+      Math.random() * max.x + min.x,
+      Math.random() * max.y + min.y,
+      Math.random() * max.z + min.z
+    ).getHexString());
+  }
+});
+
+/**
+ * Set random position within bounds.
+ */
+AFRAME.registerComponent('random-position', {
+  schema: {
+    min: {default: {x: -10, y: -10, z: -10}, type: 'vec3'},
+    max: {default: {x: 10, y: 10, z: 10}, type: 'vec3'}
+  },
+
+  update: function () {
+    var data = this.data;
+    var max = data.max;
+    var min = data.min;
+    this.el.setAttribute('position', {
+      x: Math.random() * max.x + min.x,
+      y: Math.random() * max.y + min.y,
+      z: Math.random() * max.z + min.z
+    });
+  }
+});
+
+/**
+ * Set random rotation within bounds.
+ */
+AFRAME.registerComponent('random-rotation', {
+  schema: {
+    min: {default: {x: 0, y: 0, z: 0}, type: 'vec3'},
+    max: {default: {x: 360, y: 360, z: 360}, type: 'vec3'}
+  },
+
+  update: function () {
+    var data = this.data;
+    var max = data.max;
+    var min = data.min;
+    this.el.setAttribute('rotation', {
+      x: Math.random() * max.x + min.x,
+      y: Math.random() * max.y + min.y,
+      z: Math.random() * max.z + min.z
+    });
+  }
+});
+
+/**
+ * Set random scale within bounds.
+ */
+AFRAME.registerComponent('random-scale', {
+  schema: {
+    min: {default: {x: 0, y: 0, z: 0}, type: 'vec3'},
+    max: {default: {x: 2, y: 2, z: 2}, type: 'vec3'}
+  },
+
+  update: function () {
+    var data = this.data;
+    var max = data.max;
+    var min = data.min;
+    this.el.setAttribute('scale', {
+      x: Math.random() * max.x + min.x,
+      y: Math.random() * max.y + min.y,
+      z: Math.random() * max.z + min.z
+    });
+  }
+});
