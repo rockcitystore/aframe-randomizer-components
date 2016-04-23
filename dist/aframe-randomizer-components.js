50fad5f3a064a6bbdca16021edd2223a51ae9926
Kevin Ngo <me@ngokevin.com>
2016-04-22 23:46:38 -0700

    fix random pos

--- dist/aframe-randomizer-components.js
+++ dist/aframe-randomizer-components.js
@@ -83,9 +83,9 @@
 	    var max = data.max;
 	    var min = data.min;
 	    this.el.setAttribute('position', {
-	      x: Math.random() * max.x + min.x,
-	      y: Math.random() * max.y + min.y,
-	      z: Math.random() * max.z + min.z
+	      x: Math.random() * (max.x - min.x) + min.x,
+	      y: Math.random() * (max.y - min.y) + min.y,
+	      z: Math.random() * (max.z - min.z) + min.z
 	    });
 	  }
 	});
